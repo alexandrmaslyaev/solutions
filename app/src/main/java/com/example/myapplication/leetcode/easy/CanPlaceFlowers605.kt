4525acfc7d3aa89e0662d3acb11f43090d039fb3
Alexander Maslyaev <maslyaevalexandr@gmail.com>
2021-11-26 15:18:37 +0300

    605. Can Place Flowers

--- app/src/main/java/com/example/myapplication/leetcode/easy/CanPlaceFlowers605.kt
+++ app/src/main/java/com/example/myapplication/leetcode/easy/CanPlaceFlowers605.kt
@@ -1,4 +1,22 @@
 package com.example.myapplication.leetcode.easy
 
 object CanPlaceFlowers605 {
+    fun canPlaceFlowers(flowerbed: IntArray, n: Int): Boolean {
+        var result = 0
+        var index = 0
+        while (index != flowerbed.size) {
+            val leftIsEmpty =
+                if (index == 0) true
+                else flowerbed[index - 1] == 0
+            val rightIsEmpty =
+                if (index == flowerbed.size - 1) true
+                else flowerbed[index + 1] == 0
+            if (flowerbed[index] == 0 && leftIsEmpty && rightIsEmpty) {
+                flowerbed[index] = 1
+                result++
+            }
+            index++
+        }
+        return result >= n
+    }
 }
