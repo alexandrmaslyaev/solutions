e614a950a4bf868358a32105375831be91eee9cc
Alexander Maslyaev <maslyaevalexandr@gmail.com>
2021-12-03 19:51:45 +0300

    219. Contains Duplicate

--- app/src/main/java/com/example/myapplication/leetcode/easy/ContainsDuplicate219.kt
+++ app/src/main/java/com/example/myapplication/leetcode/easy/ContainsDuplicate219.kt
@@ -1,4 +1,12 @@
 package com.example.myapplication.leetcode.easy
 
 object ContainsDuplicate219 {
+    fun containsNearbyDuplicate(nums: IntArray, k: Int): Boolean {
+        for (i in nums.indices) {
+            for (j in i + 1 until nums.size) {
+                if (nums[i] == nums[j] && Math.abs(i - j) <= k) return true
+            }
+        }
+        return false
+    }
 }
