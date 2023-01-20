3563566e62f166e6200b7b23fee45c5eba4a7a5d
Aleksandr Masliaev <a.masliaev@arammeem.com>
2023-01-20 21:49:53 +0800

    arcs ok

--- app/src/main/java/com/example/myapplication/leetcode/easy/Test.kt
+++ app/src/main/java/com/example/myapplication/leetcode/easy/Test.kt
@@ -15,38 +15,24 @@ fun Float.findNearAngle(): Float {
     }
 }
 
-@SuppressLint("LogNotTimber")
 fun Float.findNextAngle(spinClockwise: Boolean): Float {
     val signWithoutZero = if (this.sign == 0f) 1f else this.sign
-    val transformAngle: Float.() -> Float = {
-        if (spinClockwise) this - 180f else this + 180f
-    }
-    val coerceUpToLowerArc: Float.() -> Float = {
-        val minimumAngle = (abs(this@findNextAngle) / 360f).toInt() * 360f + 180f
+    val transformAngle: Float.() -> Float = { if (spinClockwise) this - 180f else this + 180f }
+    val coerce: Float.(forLowerArc: Boolean) -> Float = { forLowerArc ->
+        val minimumAngle = (abs(this@findNextAngle) / 360f).toInt() * 360f +
+            if (forLowerArc) + 180f else 0f
         val maximumAngle = minimumAngle + 180f
         coerceIn(
             minimumValue = if (signWithoutZero == 1f) minimumAngle else maximumAngle * signWithoutZero,
             maximumValue = if (signWithoutZero == 1f) maximumAngle else minimumAngle * signWithoutZero,
         )
     }
-    val coerceToUpperArc: Float.() -> Float = {
-        val minimumAngle = (this@findNextAngle / 360f).toInt() * 360f
-        val minimumValue1 = if (signWithoutZero == 1f) minimumAngle else minimumAngle - 180f
-        val maxAn = if (signWithoutZero == 1f) minimumAngle + 180f else minimumAngle
-        coerceIn(
-            minimumValue = minimumValue1,
-            maximumValue = maxAn,
-        )
-    }
     val transformedAngle = transformAngle()
     return when (abs(this@findNextAngle % 360f)) {
-        0f, 180f -> transformedAngle
-        in 180f..360f -> transformedAngle.coerceUpToLowerArc()
-        in 0f..180f -> transformedAngle.coerceToUpperArc()
-        else -> error("nepravilno ebanie volki!!!!! " +
-            "current angle = $this, " +
-            "spinClockwise = $spinClockwise "
-        )
-    }/*.also { Log.v("ROTATE", "result = $it") }*/
+        0f,
+        180f -> transformedAngle
+        in 180f..360f -> transformedAngle.coerce(true)
+        in 0f..180f -> transformedAngle.coerce(false)
+        else -> 0f
+    }
 }
-
--- app/src/main/java/com/example/myapplication/test.kt
+++ app/src/main/java/com/example/myapplication/test.kt
@@ -3,56 +3,41 @@ package com.example.myapplication
 import com.example.myapplication.leetcode.easy.*
 
 fun main() {
-    // -10 (true) -> -180 s
-    // -10 (false) -> 0 s
-    // -190 (true) -> -360 s
-    // -190 (false) -> -180 s
-    // -170 (false) -> 0 s
-    // -170 (true) -> -180 s
-    // -290 (true) -> -360 s
-    // -290 (false) -> -180 s
-    // -516 (false) -> -360 s
-    // -180 (true) -> -360 s
-    // -387 (true) -> -540 s
-    // -477 (true) -> -540 s
-    // -477 (false) -> -540 s
-    // 180 (true) -> 0 s
-    // 180 (false) -> 360 s
-    // 0 (true) -> -180 s
-    // 0 (false) -> 180 s
-    // 360 (true) -> 180 s
-    // 360 (false) -> 540 s
-    // 387 (true) -> 360 s
-    // 387 (false) -> 540 s
-    // 90 (false) -> 180 s
-    // 90 (true) -> 0 s
-    // 200 (false) -> 360 s
-    // 200 (true) -> 180 s
-    // 20 (false) -> 180f s
-    // 20 (true) -> 0f s
-    // 110 (false) -> 180f s
-    // 110 (true) -> 0f s
-    println((-566f).findNextAngle(false))
-    check((-10f).findNextAngle(true) == (-180f))
-    check((-10f).findNextAngle(false) == 0f)
-    check((-170f).findNextAngle(true) == (-180f))
-    check((-170f).findNextAngle(false) == 0f)
-    check((-190f).findNextAngle(true) == (-360f))
-    check((-190f).findNextAngle(false) == (-180f))
-    check((-290f).findNextAngle(true) == (-360f))
-    check((-290f).findNextAngle(false) == (-180f))
-    check((-387f).findNextAngle(true) == (-540f))
-    check((-387f).findNextAngle(false) == (-360f))
-    check((-536f).findNextAngle(true) == (-540f))
-    check((-536f).findNextAngle(false) == (-360f))
-//    check((-566f).findNextAngle(true) == (-720f))
-//    check((-566f).findNextAngle(false) == (-540f))
-    check(0f.findNextAngle(true) == (-180f))
+    // negative and zero
+    check(0f.findNextAngle(true) == -180f)
     check(0f.findNextAngle(false) == 180f)
-    check(360f.findNextAngle(true) == 180f)
-    check(360f.findNextAngle(false) == 540f)
+    check((-1440f).findNextAngle(true) == -1620f)
+    check((-1440f).findNextAngle(false) == -1260f)
+
+    check((-45f).findNextAngle(true) == -180f)
+    check((-45f).findNextAngle(false) == 0f)
+    check((-1505f).findNextAngle(true) == -1620f)
+    check((-1505f).findNextAngle(false) == -1440f)
+
+    check((-180f).findNextAngle(true) == -360f)
+    check((-180f).findNextAngle(false) == 0f)
+    check((-1620f).findNextAngle(true) == -1800f)
+    check((-1620f).findNextAngle(false) == -1440f)
+
+    check((-225f).findNextAngle(true) == -360f)
+    check((-225f).findNextAngle(false) == -180f)
+    check((-1685f).findNextAngle(true) == -1800f)
+    check((-1685f).findNextAngle(false) == -1620f)
+
+    // positive
+
+    check(45f.findNextAngle(true) == 0f)
+    check(45f.findNextAngle(false) == 180f)
+    check(1505f.findNextAngle(true) == 1440f)
+    check(1505f.findNextAngle(false) == 1620f)
+
     check(180f.findNextAngle(true) == 0f)
     check(180f.findNextAngle(false) == 360f)
-    check(540f.findNextAngle(true) == 360f)
-    check(540f.findNextAngle(true) == 721f)
+    check(1620f.findNextAngle(true) == 1440f)
+    check(1620f.findNextAngle(false) == 1800f)
+
+    check(225f.findNextAngle(true) == 180f)
+    check(225f.findNextAngle(false) == 360f)
+    check(1685f.findNextAngle(true) == 1620f)
+    check(1685f.findNextAngle(false) == 1800f)
 }
